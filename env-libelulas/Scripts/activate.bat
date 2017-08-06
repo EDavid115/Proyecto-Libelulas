525253e595474205e6510729686582be398b5865
Edavid115 <ernestocabanap@gmail.com>
2017-08-05 22:14:59 -0500

    Inicio proyecto
    Todos los archivos de configuración del entorno virtual y el proyecto de Django.
    SRC contiene la raiz del proyecto y toda su configuracón, en la carpeta club_libelulas se enuentra el proyecto principal.
    Requeriments contiene la información de los paquetes instalados para el proyecto.

---
+++ env-libelulas/Scripts/activate.bat
@@ -0,0 +1,32 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\erdav\OneDrive\GitHub\Proyecto-Libelulas\env-libelulas"
+
+if not defined PROMPT (
+    set "PROMPT=$P$G"
+)
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+)
+
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(env-libelulas) %PROMPT%"
+
+if defined PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+    set PYTHONHOME=
+)
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
---
+++ env-libelulas/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\erdav\onedrive\github\proyecto-libelulas\env-libelulas\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
